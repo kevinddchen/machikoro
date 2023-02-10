4ab74e9cb7862056220ea3dc9789a8c51108a68c
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2023-02-10 00:12:37 -0800

    React 18 updates

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,9 @@
-// the port we will serve from
+/**
+ * The port we will serve from
+ */
 export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 80;
 
-// true if we are in production
+/**
+ * true if we are in production
+ */
 export const IN_PROD = process.env.NODE_ENV === 'production';
