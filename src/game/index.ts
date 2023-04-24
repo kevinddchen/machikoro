c646f5a6cfdd2207b763ba35504be8377b8f4c5f
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2023-04-23 21:54:42 -0700

    Fixup

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,6 +1,6 @@
 export * from './machikoro';
 export * from './types';
-export * from './utils';
+export { expToVer } from './utils';
 export * as Est from './establishments';
 export * as Land from './landmarks';
 export * as Log from './log';
