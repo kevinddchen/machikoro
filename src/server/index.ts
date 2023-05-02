405ba24333dddb027d013e6bdc3b5eceb440af09
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2023-05-02 00:05:27 -0700

    debug

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,7 +4,6 @@ import serve from 'koa-static';
 
 import { Machikoro } from '../game';
 import { PORT } from '../common/config';
-import { addCustomMiddleware } from './middleware';
 
 // game server
 const server = Server({
@@ -16,9 +15,6 @@ const server = Server({
     Origins.LOCALHOST_IN_DEVELOPMENT,
   ],
 });
-
-// run our middleware before any of boardgame.io's middleware
-addCustomMiddleware(server);
 
 // Build path relative to this file
 const frontEndAppBuildPath = path.resolve(__dirname, '../../build');
