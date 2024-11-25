1c8b7bccc54289c5ee5bd40967a15069cf84f8cc
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2024-11-25 15:42:41 -0800

    Add webpack (#149)
    * add webpack
    
    * fix css
    
    * fix static assets
    
    * fixup
    
    * fix: lint

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,9 +1,4 @@
 /**
- * The port we will serve from
- */
-export const PORT = parseInt(process.env.PORT ?? '80');
-
-/**
  * true if we are in production
  */
 export const IN_PROD = process.env.NODE_ENV === 'production';
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './public',
+          globOptions: {
+            ignore: [
+              '**/*.html', // Ignore all `html` files
+            ],
+          },
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+};
