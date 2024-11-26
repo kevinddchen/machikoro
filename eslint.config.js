06f9402a9420b9cdbefbf1c49d4945849ecd72b9
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2024-11-26 00:15:42 -0800

    use flat config for eslint

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+const eslint = require("@eslint/js");
+const react = require('eslint-plugin-react');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+  {
+    ignores: ['build/', 'dist/'],
+  },
+  {
+    extends: [
+      eslint.configs.recommended,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
+      tseslint.configs.strict,
+      tseslint.configs.stylistic,
+    ],
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      react,
+    },
+    rules: {
+      'sort-imports': [
+        'error',
+        {
+          allowSeparatedGroups: true,
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+)
